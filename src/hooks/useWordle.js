91718d967ca963f1747e5629d29ad35f6410f58f
joycehwchan <joycehwchan@gmail.com>
2023-02-26 14:03:31 +0900

    setup custom hook

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const useWordle = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([]); // each guess is an array
+  const [history, setHistory] = useState([]); // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  // format a guess into an array of letter objects
+  const formatGuess = () => {};
+
+  // add new guess to the guesses state
+  // update the isCorrect state if guess is correct
+  // add one to the turn state
+  const addNewGuess = () => {};
+
+  // handle keyup event & track current guess
+  // add new guess when user presses enter
+  const handleKeyup = () => {};
+
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+};
+
+export default useWordle;
