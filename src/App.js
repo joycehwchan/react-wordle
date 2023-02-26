44ee8c69755368072e4e2c8440ce8f817005138f
joycehwchan <joycehwchan@gmail.com>
2023-02-26 14:32:25 +0900

    handleKeyup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { words } from "./words";
+import Wordle from "./components/Wordle";
 
 function App() {
   const [solution, setSolution] = useState(null);
@@ -8,12 +9,12 @@ function App() {
     const randomWord = words[Math.floor(Math.random() * words.length)];
     setSolution(randomWord.word);
     console.log(randomWord);
-  }, []);
+  }, [setSolution]);
 
   return (
     <div className="App">
       <h1>Wordle</h1>
-      <p>Solution: {solution}</p>
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -8,7 +8,9 @@ const useWordle = (solution) => {
   const [isCorrect, setIsCorrect] = useState(false);
 
   // format a guess into an array of letter objects
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("Formatting...", currentGuess);
+  };
 
   // add new guess to the guesses state
   // update the isCorrect state if guess is correct
@@ -17,7 +19,38 @@ const useWordle = (solution) => {
 
   // handle keyup event & track current guess
   // add new guess when user presses enter
-  const handleKeyup = () => {};
+  const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      if (turn > 5) {
+        console.log("you run out of guesses");
+        return;
+      }
+
+      if (history.includes(currentGuess)) {
+        console.log("you already tried this word");
+        return;
+      }
+
+      if (currentGuess.length !== 5) {
+        console.log("word needs to have 5 letters");
+        return;
+      }
+      formatGuess();
+    }
+
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
+    }
+
+    if (/^[A-Za-z]$/.test(key) && currentGuess.length < 5) {
+      setCurrentGuess((prev) => {
+        return prev + key;
+      });
+    }
+  };
 
   return { turn, currentGuess, guesses, isCorrect, handleKeyup };
 };
