3e6bda1b38056267458b895b20c18c65c44a88a6
joycehwchan <joycehwchan@gmail.com>
2023-02-27 20:22:30 +0900

    display game grid

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn } =
@@ -22,6 +23,7 @@ export default function Wordle({ solution }) {
     <div>
       <div>current guess: {currentGuess}</div>
       <div>solution: {solution}</div>
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
     </div>
   );
 }
